5531293590d3077be54cdbadbe2035dd88c1be9c
Shurjo Banerjee <shurjo@umich.edu>
2019-03-06 00:58:39 -0500

    prelim experiments

---
+++ baselines/all.sh
@@ -0,0 +1,14 @@
+ python run_her.py --num_timesteps 150000  --hidden 16 --n_arms 2  --env Arm
+ python run_her.py --num_timesteps 150000  --hidden 16 --n_arms 2 --parts diff --env Arm
+ 
+ python run_her.py --num_timesteps 150000  --hidden 16 --n_arms 6  --env Arm
+ python run_her.py --num_timesteps 150000  --hidden 16 --n_arms 6 --parts diff --env Arm
+ 
+ python run_her.py --num_timesteps 150000  --hidden 16 --n_arms 15  --env Arm
+ python run_her.py --num_timesteps 150000  --hidden 16 --n_arms 15 --parts diff --env Arm
+ 
+ python run_her.py --num_timesteps 150000  --hidden 256 --env Hand
+ python run_her.py --num_timesteps 150000  --hidden 212 --parts diff --env Hand
+ 
+ python run_her.py --num_timesteps 150000  --hidden 256 --env Fetch
+ python run_her.py --num_timesteps 150000  --hidden 212 --parts diff --env Fetch
